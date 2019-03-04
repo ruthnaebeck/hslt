963bc58663f11d4a70de407f3c106863c6188fe0
Ruth Naebeck <ruthnaebeck@gmail.com>
2019-03-03 23:57:50 -0500

    update to text input

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,13 @@
+var player = document.getElementById('player');
+player.addEventListener('keyup', function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById('go').click();
+  }
+});
+
 function setIframeSource() {
-  var player = document.getElementById('player');
-  playerName = player.options[player.selectedIndex].text.toLowerCase();
+  var playerName = player.value.toLowerCase();
   var url = `https://app.datadoghq.com/graph/embed?token=891f240a90034e20765e89981ff961254898da9647954a0000079a179f2910bf&height=400&width=800&legend=true&season=*&player=player:${playerName}`;
 
   var iframe = document.getElementById('rank-for-player');
