924766da0ca99451244907a268d9f8bc11fab2d2
Ruth Naebeck <ruthnaebeck@gmail.com>
2019-03-03 23:29:02 -0500

    make url selection dynamic

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 function setIframeSource() {
-  var dropdown = document.getElementById('player');
+  var player = document.getElementById('player');
+  playerName = player.options[player.selectedIndex].text.toLowerCase();
+  var url = `https://app.datadoghq.com/graph/embed?token=891f240a90034e20765e89981ff961254898da9647954a0000079a179f2910bf&height=400&width=800&legend=true&season=*&player=player:${playerName}`;
+
   var iframe = document.getElementById('rank-for-player');
-  var url = dropdown.options[dropdown.selectedIndex].value;
   iframe.src = url;
 }
